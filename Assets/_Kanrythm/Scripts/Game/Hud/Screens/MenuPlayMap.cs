f6b0a52148b2301c13808c6c8441f31b69b8184e
Knose1 <34002600+Knose1@users.noreply.github.com>
2019-11-11 14:57:53 +0100

    Finished hotfix

--- Assets/_Kanrythm/Scripts/Game/Hud/Screens/MenuPlayMap.cs
+++ Assets/_Kanrythm/Scripts/Game/Hud/Screens/MenuPlayMap.cs
@@ -1,3 +1,4 @@
+using Com.Github.Knose1.Common;
 using Com.Github.Knose1.Kanrythm.Game.Hud;
 using Com.Github.Knose1.Kanrythm.Loader;
 using System;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace Com.Github.Knose1.Kanrythm.Game.Hud.Screens
@@ -18,19 +20,29 @@ namespace Com.Github.Knose1.Kanrythm.Game.Hud.Screens
 		[NonSerialized] public uint mapId = 0;
 		[NonSerialized] public uint diffId = 0;
 
+		#region HudContainer Down events
 		public override void OnAddedToHudContainer(HudContainer hudContainer)
 		{
 			base.OnAddedToHudContainer(hudContainer);
 			buttonPlay				.onClick.AddListener(Button_OnClick_Play);
 			buttonPlayInAutoplayMode.onClick.AddListener(Button_OnClick_Autoplay);
-		}
 
+			Controller.Instance.Input.Hud.Exit.performed += Exit_performed;
+		}
 		public override void OnRemovedFromHudContainer(HudContainer hudContainer)
 		{
 			buttonPlay				.onClick.RemoveAllListeners();
 			buttonPlayInAutoplayMode.onClick.RemoveAllListeners();
 
+			Controller.Instance.Input.Hud.Exit.performed -= Exit_performed;
+
 			base.OnRemovedFromHudContainer(hudContainer);
+		}
+		#endregion
+
+		private void Exit_performed(InputAction.CallbackContext obj)
+		{
+			HudContainer.SetScreen(HudManager.Instance.GetTemplateMenu());
 		}
 
 		private void Button_OnClick_Play()
