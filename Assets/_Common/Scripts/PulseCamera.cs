03004f4e40ae8c09aa3cc517e002bf97065d0163
Knose1 <34002600+Knose1@users.noreply.github.com>
2019-12-15 22:42:53 +0100

    push

---
+++ Assets/_Common/Scripts/PulseCamera.cs
@@ -0,0 +1,51 @@
+///-----------------------------------------------------------------
+/// Author : AymerickHebert
+/// Date : 26/11/2019 15:54
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.Github.Knose1.Common {
+	public class PulseCamera : MonoBehaviour {
+
+		public Camera cameraToPulse;
+
+		private float minFOE = 0;
+		public float deltaFOE = 10;
+		public AnimationCurve pulseCurve = AnimationCurve.Linear(0,0,1,1);
+		public bool fromMinToMax = true;
+		public bool pingPong = false;
+		[Tooltip("The time for one pulse in seconds")] public float pulseTime = 1;
+
+		private float timestamp = 0;
+
+		private void Start()
+		{
+			minFOE = cameraToPulse.fieldOfView;
+
+			timestamp = -pulseTime;
+		}
+
+		private void Update ()
+		{
+			float lElapsedTime = StretchableDeltaTime.Instance.ElapsedTime;
+			float lLerp = (fromMinToMax ? 1 : -1) * (lElapsedTime - timestamp) / pulseTime;
+
+			if (!fromMinToMax) lLerp += 1;
+
+
+			if			(lLerp >= 1 && fromMinToMax)	cameraToPulse.fieldOfView = minFOE;
+			else if		(lLerp <= 0 && !fromMinToMax)	cameraToPulse.fieldOfView = minFOE + deltaFOE;
+			else										cameraToPulse.fieldOfView = Mathf.Lerp(minFOE, minFOE + deltaFOE, pulseCurve.Evaluate(lLerp));
+
+		}
+
+		public void Pulse()
+		{
+			timestamp = StretchableDeltaTime.Instance.ElapsedTime;
+
+			if (pingPong)
+				fromMinToMax = !fromMinToMax;
+		}
+	}
+}
---
+++ Assets/_Common/Scripts/StretchableDeltaTime.cs
@@ -0,0 +1,89 @@
+using Com.Github.Knose1.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Com.Github.Knose1.Common
+{
+	public class StretchableDeltaTime : StateMachine, IDisposable
+	{
+		private float startTime = -1;
+		private float scaleTime = 1;
+		private float elapsedTime = 0;
+		private float scaledDeltaTime;
+
+		private static StretchableDeltaTime instance;
+
+		public static StretchableDeltaTime Instance { get => instance; }
+
+		public float StartTime { get => startTime; }
+		public float ScaleTime { get => scaleTime; set => scaleTime = Math.Max(0.001f, value); }
+		public float ElapsedTime { get => elapsedTime; }
+		public float ScaledDeltaTime { get => scaledDeltaTime; }
+		public bool IsPlaying { get => doAction != DoActionVoid; }
+
+		public StretchableDeltaTime() {
+			if (instance) instance.Dispose();
+
+			instance = this;
+		}
+
+		public void StartDeltaTime()
+		{
+			startTime = Time.time;
+			doAction = DoActionNormal;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void TogglePauseDeltaTime()
+		{
+			TogglePauseDeltaTime(doAction == DoActionNormal);
+		}
+		public void TogglePauseDeltaTime(bool setToPause)
+		{
+			if (!setToPause)
+			{
+				doAction = DoActionNormal;
+			}
+			else
+			{
+				doAction = DoActionVoid;
+			}
+		}
+
+		public void StopDeltaTime()
+		{
+			doAction = DoActionVoid;
+			startTime = -1;
+			scaleTime = 1;
+			elapsedTime = 0;
+		}
+
+		protected override void DoActionNormal()
+		{
+			base.DoActionNormal();
+
+			float lNewDelta = Time.deltaTime * scaleTime;
+
+			scaledDeltaTime = lNewDelta;
+			elapsedTime += lNewDelta;
+
+		}
+
+		public void Dispose()
+		{
+			instance = null;
+			Destroy(this);
+		}
+
+		public float GetElapsedTimeRatio(float start, float targetTime)
+		{
+			return (ElapsedTime - start) / targetTime;
+		}
+	}
+}
